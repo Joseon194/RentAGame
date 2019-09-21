46d61bf9a7d2116323e6510664052c705ebb3ecd
Dennis Chung <chungdennis6@gmail.com>
2019-09-21 07:14:34 +0000

    added lines to sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def login
+    already_logged_in?
+    @user = User.new
+end
+
+def create
+  @user = User.find_by(name: params[:user][:name])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect_to home_path(@user)
+  else
+    render :login
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_path
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :to_already_logged_in?, except: [:home, :reviewed_games]
+
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new
+  if @user.valid? && @user.save
+    session[:user_id] = @user.id
+    redirect_to home_path(@user)
+  else
+    render :new
+  end
+end
+
+def home
+  user_not_authenticated?
+  current_user
+end
+
+def reviewed_games
+  @user = current_user
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
 end
