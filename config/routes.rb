621199c2c306fc9dc61682cfa6a7f5e080870bed
Dennis Chung <chungdennis6@gmail.com>
2019-09-28 16:16:56 +0000

    created omniauth routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  resources :users
+
+ resources :games do
+   resources :reviews
+ end
+ get '/home' => 'users#home'
+ get '/collection' => 'games#collection'
+ get '/login' => 'sessions#login'
+ post '/login' => 'sessions#create'
+ post '/logout' => 'sessions#destroy'
+
+ root 'welcome#home'
+
+ get '/auth/facebook/callback' => 'sessions#facebook_create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
