4653bb187ee62844d8c3d1f5795a030dea5f84b5
Joseon194 <chungdennis6@gmail.com>
2020-01-05 17:14:46 -0800

    changed line 6 in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do  
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
     provider :developer
-    provider :facebook ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+    provider :facebook
   end
