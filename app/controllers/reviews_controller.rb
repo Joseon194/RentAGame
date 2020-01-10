74cb0f9c689a59fdfb9822fc4770d567795424e6
Joseon194 <chungdennis6@gmail.com>
2020-01-09 20:46:54 -0800

    added lines to review_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,26 @@
 class ReviewsController < ApplicationController
+    def new
+        @game = Game.find_by_id(params[:game_id])
+    end
+
   def create
-       review = Review.create(review_params)
-       redirect_to review.game #need to redirect to show
+       # review = Review.create(review_params)
+       @game = Game.find_by_id(params[:game_id])
+        @review = @game.reviews.create(review_params)
+       redirect_to game_path(@game) #need to redirect to show
    end
+
+  def edit
+    @review = Review.find_by_id(params[:id])
+    if 
+    @review.user != current_user
+    redirect_to review.game(user.id)
+  end
+end
+
+  def index
+    @game = Game.find_by_id(params[:game_id])
+  end
 
    private
    def review_params
